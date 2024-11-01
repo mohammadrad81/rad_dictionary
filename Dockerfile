50bfa81c1ce57d690a4c10200597b969edb20450
mohammadrad81 <mohammad.rad81@gmail.com>
2024-11-01 15:04:15 +0330

    cache implemented

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.12-alpine
+COPY ./requirements.txt .
+RUN pip install -r requirements.txt
+RUN mkdir application
+COPY main.py application
+COPY .env application
+WORKDIR application
+# CMD ["ls", "-lath", "app"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -17,8 +17,13 @@ CACHING_TIME = os.getenv("CACHING_TIME")
 API_KEY = os.getenv("API_KEY")
 RANDOM_WORD_SERVICE = os.getenv("RANDOM_WORD_SERVICE")
 WORD_MEANING_SERVICE = os.getenv("WORD_MEANING_SERVICE")
+REDIS_CACHE_IP = os.getenv("REDIS_CACHE_IP")
+REDIS_CACHE_PORT = os.getenv("REDIS_CACHE_PORT")
+print(REDIS_CACHE_IP)
+print(REDIS_CACHE_PORT)
 
 app = Flask(__name__)
+cache = Redis(REDIS_CACHE_IP, REDIS_CACHE_PORT)
 
 def get_random_word() -> str:
     headers = {
@@ -28,7 +33,10 @@ def get_random_word() -> str:
     word = json.loads(result.content.decode())["word"][0]
     return word
 
-def get_meaning_of_word(word: str) -> str:
+def get_meaning_of_word(word: str) -> tuple[str, bool]:
+    cached_value = cache.get(word)
+    if cached_value is not None:
+        return cached_value.decode(), True
     headers = {
         "x-api-key": API_KEY
     }
@@ -37,7 +45,8 @@ def get_meaning_of_word(word: str) -> str:
     }
     result = requests.get(WORD_MEANING_SERVICE, headers=headers, params=params)
     meaning = json.loads(result.content.decode())["definition"]
-    return meaning
+    cache.set(word, meaning)
+    return meaning, False
 
 @app.route("/")
 def liveness():
@@ -47,23 +56,24 @@ def liveness():
 @app.route("/meaning/")
 def meaning_of_word():
     word = request.args.get("word")
-    meaning = get_meaning_of_word(word)
+    meaning, from_cache = get_meaning_of_word(word)
     result = {
         "word": word,
-        "meaning": meaning
+        "meaning": meaning,
+        "from_cache": from_cache
     }
     return result
 
 @app.route("/random-word-with-meaning/")
 def random_word_with_meaning():
     random_word = get_random_word()
-    meaning = get_meaning_of_word(random_word)
+    meaning, from_cache = get_meaning_of_word(random_word)
     result = {
         "random_word": random_word,
-        "meaning": meaning
+        "meaning": meaning,
+        "from_cache": from_cache
     }
     return result
 
 if __name__ == "__main__":
-    # print(get_meaning_of_word("interest"))
     app.run("0.0.0.0", SERVER_PORT)
